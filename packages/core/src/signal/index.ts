f448989b429ad5eb8322927b759c368ee9eb0cc0
jiangxd <jiangxd2016@gmail.com>
2024-05-09 21:03:16 +0800

    feat(signal): export isSignal isComputed utility function

--- packages/core/src/signal/index.ts
+++ packages/core/src/signal/index.ts
@@ -1,3 +1,3 @@
-export { useSignal, useEffect, useComputed, signalObject } from './signal';
-export { type Signal } from './signal';
+export { useSignal, useEffect, useComputed, signalObject, isSignal, isComputed } from './signal';
+export { type Signal, type Computed } from './signal';
 export { createStore, StoreActions } from './store';
--- packages/core/src/template/index.ts
+++ packages/core/src/template/index.ts
@@ -2,4 +2,4 @@ export { TemplateNode } from './template-node';
 export { ComponentNode } from './component-node';
 export { h, template, Fragment, isJsxElement } from './template';
 export { nextTick } from './utils';
-export { onMount, onDestroy, useInject, useProvide, useRef } from './hooks';
+export { onMount, onDestroy, useInject, useProvide, useRef, InjectionKey } from './hooks';
