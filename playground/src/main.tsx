560a60d0bcd086d2ada78d2c033158c2bf0f8ffd
xdjiang <jiangxd2016@gmail.com>
2024-05-24 16:57:18 +0800

    fix: delete playground test code (#57)

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,20 +1,4 @@
-import { createStore, reactive } from 'essor';
-
-const sharedStore = createStore({
-  state: {
-    value: 'hello',
-  },
-  actions: {
-    updateValue(value) {
-      this.value = value;
-    },
-  },
-  getters: {
-    doubleValue() {
-      return this.value + this.value;
-    },
-  },
-});
+import { reactive } from 'essor';
 
 function Component(props) {
   return (
