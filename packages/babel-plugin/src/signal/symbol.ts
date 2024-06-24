7c8b2f4cfd934daf77f110822a29261c24921037
jiangxd <jiangxd2016@gmail.com>
2024-06-24 18:23:09 +0800

    feat: try symbal add value(WIP)

--- packages/babel-plugin/src/signal/symbol.ts
+++ packages/babel-plugin/src/signal/symbol.ts
@@ -3,7 +3,16 @@ import { type Identifier, type VariableDeclarator, cloneNode } from '@babel/type
 import { startsWith } from 'essor-shared';
 import { imports } from '../program';
 import type { NodePath } from '@babel/core';
-
+/**
+ * 当一个变量/常量声明的时候，判断是否是$(定义的符号）开头，如果是，则进行转换。
+ *
+ * case 1: let $a = 1 => let $a = useSignal(1);
+ * case 2: const $a = ()=>{return $a} => const $a = useComputed(()=>{return $a})
+ *
+ *  并且所有当前用到的值，都会添加.value
+ * @param path
+ * @returns {void}
+ */
 export function replaceSymbol(path: NodePath<VariableDeclarator>) {
   const init = path.node.init;
   const variableName = (path.node.id as Identifier).name;
