65df8bd2e13be0211e978eeceae8dc0bb66b45c9
xdjiang <jiangxd2016@gmail.com>
2024-06-26 16:29:37 +0800

    89 bug template render shorted bug (#90)
    * fix(babel-plugin): fix bable jsx self close tag in has sibling
    
    * fix(babel-plugin): fix bable jsx self close tag in has sibling

--- playground/src/main.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  let $v = 1;
-  return (
-    <>
-      <input type="text" bind:value={$v} step={1} updateValue={v => ($v = v)} />
-    </>
-  );
-}
-(<App />).mount(document.querySelector('#app')!);
