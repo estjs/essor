f83ad3e856b218e6c4c0b99a0df30205167d543f
jiangxd <jangxd2016@gmail.com>
2024-07-01 18:01:53 +0800

    fix(babel): fix babel symbol transfrom check is has .value

--- examples/basic/src/main.tsx
+++ examples/basic/src/main.tsx
@@ -1,8 +1,28 @@
+function Com2({ ...rest }) {
+  return (
+    <div>
+      {rest.v}
+      {rest.val}
+      {rest.xxx}
+    </div>
+  );
+}
+function Com({ ...rest }) {
+  return (
+    <div>
+      {rest.v}
+      {rest.val}
+      {rest.xxx}
+      <Com2 v={rest.v} val={1} xxx={123}></Com2>
+    </div>
+  );
+}
+
 function App() {
   const $v = 1;
   return (
     <>
-      <p>{$v}</p>
+      <Com v={$v} val={1} xxx={123}></Com>;
       <input type="text" bind:value={$v} />
     </>
   );
---
+++ packages/babel-plugin/README.md
@@ -0,0 +1,14 @@
+# Essor babel plugin
+
+## 安装
+
+```bash
+npm install @essor/babel-plugin --save-dev
+```
+
+## 使用
+
+
+::: warn
+不要在pops中使用$,props是个reactive,必须添加value
+:::
