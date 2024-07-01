4c7658a7887dd9a1f5ca2252161a1d851f367bfd
jiangxd <jangxd2016@gmail.com>
2024-07-01 14:29:35 +0800

    feat: update symbol replace

--- packages/babel-plugin/src/signal/symbol.ts
+++ packages/babel-plugin/src/signal/symbol.ts
@@ -1,15 +1,16 @@
 import { types as t } from '@babel/core';
-import { type Identifier, type VariableDeclarator, cloneNode } from '@babel/types';
 import { startsWith } from 'essor-shared';
 import { imports } from '../program';
+import type { Identifier, VariableDeclarator } from '@babel/types';
 import type { NodePath } from '@babel/core';
+
 /**
  * 当一个变量/常量声明的时候，判断是否是$(定义的符号）开头，如果是，则进行转换。
  *
  * case 1: let $a = 1 => let $a = useSignal(1);
  * case 2: const $a = ()=>{return $a} => const $a = useComputed(()=>{return $a})
  *
- *  并且所有当前用到的值，都会添加.value
+ * 并且所有当前用到的值，都会添加.value
  * @param path
  * @returns {void}
  */
@@ -35,27 +36,64 @@ export function replaceSymbol(path: NodePath<VariableDeclarator>) {
     imports.add('useComputed');
     path.node.init = newInit; // 直接替换 AST 节点
   } else {
-    // 判断参数是否是基本数据类型 ，也可能没有参数
-    const originalImportDeclarationNodes = cloneNode(path.get('id').node, true);
-
     const newInit = t.callExpression(t.identifier(path.state.useSignal.name), init ? [init] : []);
     imports.add('useSignal');
     path.node.init = newInit;
-
-    path.scope.rename(variableName, `${variableName}.value`);
-
-    path.get('id').replaceWith(originalImportDeclarationNodes);
-
-    // // 这里需要确保只修改在当前作用域中的变量名
-    // path.scope.traverse(path.scope.block, {
-    //   Identifier(innerPath) {
-    //     if (t.isExportSpecifier(innerPath.parent)) {
-    //       const { name } = innerPath.node;
-    //       if (name.endsWith('.value')) {
-    //         innerPath.node.name = name.slice(0, -6); // 删除 '.value' 部分
-    //       }
-    //     }
-    //   },
-    // });
   }
+}
+
+export function symbolIdentifier(path) {
+  // 忽略变量声明和导入声明
+  if (
+    path.parentPath.isVariableDeclarator() ||
+    path.parentPath.isImportSpecifier() ||
+    path.parentPath.isObjectProperty() ||
+    path.parentPath.isArrayPattern() ||
+    path.parentPath.isObjectPattern()
+  ) {
+    return;
+  }
+
+  const { node } = path;
+
+  if (node.name.startsWith('$')) {
+    const uniqueId = path.scope.generateUidIdentifierBasedOnNode(node, '$');
+    path.scope.rename(node.name, uniqueId.name);
+
+    const newNode = t.memberExpression(t.identifier(uniqueId.name), t.identifier('value'));
+    path.replaceWith(newNode);
+  }
+}
+
+export function symbolObjectPattern(path) {
+  path.node.properties.forEach(property => {
+    if (
+      t.isObjectProperty(property) &&
+      t.isIdentifier(property.key) &&
+      property.key.name.startsWith('$')
+    ) {
+      const newKey = t.identifier(property.key.name);
+      property.key = newKey;
+    }
+  });
+}
+
+export function symbolArrayPattern(path) {
+  path.node.elements.forEach(element => {
+    if (t.isIdentifier(element) && element.name.startsWith('$')) {
+      const newElement = t.identifier(element.name);
+      element.name = newElement.name;
+    } else if (t.isObjectPattern(element)) {
+      element.properties.forEach(property => {
+        if (
+          t.isObjectProperty(property) &&
+          t.isIdentifier(property.key) &&
+          property.key.name.startsWith('$')
+        ) {
+          const newKey = t.identifier(property.key.name);
+          property.key = newKey;
+        }
+      });
+    }
+  });
 }
