6c1cca1191f844004c6741dbe4869ed18f118979
jiangxd <jiangxd2016@gmail.com>
2024-09-01 19:06:31 +0800

    feat: delete example provide console

--- examples/provide/src/inject.tsx
+++ examples/provide/src/inject.tsx
@@ -3,6 +3,5 @@ import { ProvideKey } from './main';
 
 export default function InjectComponent() {
   const injectVlaue = useInject(ProvideKey, { count: -1 })!;
-  console.log(injectVlaue);
   return <div>{injectVlaue.count}</div>;
 }
