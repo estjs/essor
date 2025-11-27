e5c29c09b11c79df351437ed101eb27e1e7b0faf
jiangxd <jiangxd2016@gmail.com>
2025-11-27 10:24:24 +0800

    feat:  verify commit support breaking change

--- scripts/verify-commit.ts
+++ scripts/verify-commit.ts
@@ -8,7 +8,7 @@ const msgPath = path.resolve('.git/COMMIT_EDITMSG');
 const msg = readFileSync(msgPath, 'utf-8').trim();
 
 const commitRE =
-  /^(?:revert: )?(?:feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(?:\(.+\))?: .{1,50}/;
+  /^(?:revert: )?(?:feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(?:\(.+\))?!?: .{1,50}/;
 
 if (!commitRE.test(msg)) {
   console.log();
@@ -18,9 +18,9 @@ if (!commitRE.test(msg)) {
     )}\n\n${pico.red(
       '  Proper commit message format is required for automated changelog generation. Examples:\n\n',
     )}    ${pico.green(`feat(core): add 'comments' option`)}\n` +
-      `    ${pico.green('fix(signal): handle events on blur (close #28)')}\n\n${pico.red(
-        '  See .github/commit-convention.md for more details.\n',
-      )}`,
+    `    ${pico.green('fix(signal): handle events on blur (close #28)')}\n\n${pico.red(
+      '  See .github/commit-convention.md for more details.\n',
+    )}`,
   );
   process.exit(1);
 }
