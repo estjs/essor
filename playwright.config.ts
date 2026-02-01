16916db02b82214e77929ec40db2003b2bdff550
jiangxd2016 <jiangxd2016@gmail.com>
2026-02-01 22:15:51 +0800

    chore: update test retry configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -51,7 +51,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },  
 
     /* Test against mobile viewports. */
     // {
